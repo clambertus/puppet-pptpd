37ff6c43f894ac713166415b068477b9a43eaceb
Chris Lambertus <cml@apache.org>
2015-03-25 12:04:13 -0700

    add pptpd_options parameter, fix spacing

--- manifests/init.pp
+++ manifests/init.pp
@@ -18,22 +18,24 @@
 # Alexander Fortin <alexander.fortin@gmail.com>
 #
 class pptpd (
-  $ensure      = 'present',
-  $option      = '/etc/ppp/pptpd-options',
-  $stimeout    = 10,
-  $debug       = false,
-  $bcrelay     = false,
-  $connections = 100,
-  $delegate    = true,
-  $localip     = $::ipaddress,
-  $remoteip    = '192.168.0.234-238,192.168.0.245',
-  $dns_servers = [],
-  $nmb_servers = [],
-  $noipparam   = false,
-  $listen      = false,
-  $pidfile     = '/var/run/pptpd.pid',
-  $speed       = 115200,
-  $logwtmp     = true
+  $ensure        = 'present',
+  $option        = '/etc/ppp/pptpd-options',
+  $stimeout      = 10,
+  $debug         = false,
+  $bcrelay       = false,
+  $connections   = 100,
+  $delegate      = true,
+  $localip       = $::ipaddress,
+  $remoteip      = '192.168.0.234-238,192.168.0.245',
+  $dns_servers   = [],
+  $nmb_servers   = [],
+  $noipparam     = false,
+  $listen        = false,
+  $pidfile       = '/var/run/pptpd.pid',
+  $speed         = 115200,
+  $logwtmp       = true,
+  $pptpd_options = [],
+
   ) {
 
   include 'pptpd::package'
